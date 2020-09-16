9c2c624556c2bce68a8dec7395a651f90e9bcbb3
Danny Coates <dannycoates@gmail.com>
2020-09-16 10:20:43 -0700

    added fetching for missing checksums

--- sources/index.ts
+++ sources/index.ts
@@ -1,5 +1,5 @@
 import { WorkspaceRequiredError } from "@yarnpkg/cli";
-import {CommandContext, Plugin, Configuration, Project, Cache, ThrowReport} from '@yarnpkg/core';
+import {CommandContext, Plugin, Configuration, Project, Cache, ThrowReport, LocatorHash, miscUtils, structUtils} from '@yarnpkg/core';
 import {Command} from 'clipanion';
 
 class RelockCommand extends Command<CommandContext> {
@@ -23,6 +23,33 @@ class RelockCommand extends Command<CommandContext> {
       cache,
       report: new ThrowReport()
     })
+
+    // fetch packages with missing checksums
+    try {
+      const missingChecksumLocators = Array.from(
+        new Set(
+          miscUtils.sortMap(project.storedResolutions.values(), [
+            (locatorHash: LocatorHash) => {
+              const pkg = project.storedPackages.get(locatorHash);
+              if (!pkg)
+                throw new Error(`Assertion failed: The locator should have been registered`);
+
+              return structUtils.stringifyLocator(pkg);
+            },
+          ])
+        )
+      ).filter(lh => !project.storedChecksums.has(lh));
+      for (const locatorHash of missingChecksumLocators) {
+        const pkg = project.storedPackages.get(locatorHash);
+        const fetcher = configuration.makeFetcher();
+        const result = await fetcher.fetch(pkg, {checksums: project.storedChecksums, project, cache, fetcher, report: new ThrowReport()});
+        if (result.checksum) {
+          project.storedChecksums.set(pkg.locatorHash, result.checksum);
+        }
+      }
+    }
+    catch (e) {}
+
     await project.persistLockfile()
   }
 }
